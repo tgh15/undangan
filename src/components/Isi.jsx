4e3e1c77911a41405704898cb6fddcd3636183ff
tgh15 <muhteguhadhiputra15@gmail.com>
2022-05-12 10:47:03 +0800

    mantap

--- src/components/Isi.jsx
+++ src/components/Isi.jsx
@@ -40,19 +40,47 @@ function Isi() {
       </p>
       <h1 className="text-5xl pb-3 text mt-3">Save The Date</h1>
       {/* <div className="bg-white rounded-md shadow-md p-4 w-full text mb-4">
-                <h1 className="text-2xl font-bold">Akad</h1>
-                <h1 className="text-xl text-red-600 font-semibold">Selasa, 17 Mei 2022 | 09.15 WITA</h1>
-                <h1 className="text-md text-red-600 font-semibold">Masjid Al-Markaz Al-Islami Makassar</h1>
-                <iframe className="w-full mt-3" title="akad" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.8367533192504!2d119.42416111476398!3d-5.129981796277049!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbefd5a5f2dece3%3A0x7a490939f4f38e49!2sMasjid%20Al-Markaz%20Al-Islami%20Makassar!5e0!3m2!1sid!2sid!4v1652273481755!5m2!1sid!2sid"  height="250" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
-            <div className="bg-white rounded-md shadow-md p-4 w-full text mb-4">
-                <h1 className="text-2xl font-bold">Resepsi</h1>
-                <h1 className="text-xl text-red-600 font-semibold">Rabu, 18 Mei 2022 | 10.00 WITA</h1>
-                <h1 className="text-sm text-red-600 font-semibold">BTP, JL. Kemuliaan Bangkala Blok D No. 58</h1>
-                <h1 className="text-sm text-red-600 font-semibold">(Lorong TK Islam Terpadu Al Fikri)</h1>
-                <h1 className="text-sm text-red-600 font-semibold">(Penjahit Kiran)</h1>
-                <iframe className="w-full" title="resepsi" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.8015284192406!2d119.50999951434078!3d-5.135635953382417!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbefbc7d33d3b57%3A0x1a31d1516963bcff!2sJalan%20Kemuliaan%20Bangkala%20BTP!5e0!3m2!1sid!2sid!4v1652274397217!5m2!1sid!2sid" height="250" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div> */}
+        <h1 className="text-2xl font-bold">Akad</h1>
+        <h1 className="text-xl text-red-600 font-semibold">
+          Selasa, 17 Mei 2022 | 09.15 WITA
+        </h1>
+        <h1 className="text-md text-red-600 font-semibold">
+          Masjid Al-Markaz Al-Islami Makassar
+        </h1>
+        <iframe
+          className="w-full mt-3"
+          title="akad"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.8367533192504!2d119.42416111476398!3d-5.129981796277049!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbefd5a5f2dece3%3A0x7a490939f4f38e49!2sMasjid%20Al-Markaz%20Al-Islami%20Makassar!5e0!3m2!1sid!2sid!4v1652273481755!5m2!1sid!2sid"
+          height="250"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+      <div className="bg-white rounded-md shadow-md p-4 w-full text mb-4">
+        <h1 className="text-2xl font-bold">Resepsi</h1>
+        <h1 className="text-xl text-red-600 font-semibold">
+          Rabu, 18 Mei 2022 | 10.00 WITA
+        </h1>
+        <h1 className="text-sm text-red-600 font-semibold">
+          BTP, JL. Kemuliaan Bangkala Blok D No. 58
+        </h1>
+        <h1 className="text-sm text-red-600 font-semibold">
+          (Lorong TK Islam Terpadu Al Fikri)
+        </h1>
+        <h1 className="text-sm text-red-600 font-semibold">(Penjahit Kiran)</h1>
+        <iframe
+          className="w-full"
+          title="resepsi"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.8015284192406!2d119.50999951434078!3d-5.135635953382417!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbefbc7d33d3b57%3A0x1a31d1516963bcff!2sJalan%20Kemuliaan%20Bangkala%20BTP!5e0!3m2!1sid!2sid!4v1652274397217!5m2!1sid!2sid"
+          height="250"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div> */}
       <div className="grid grid-cols-2 gap-3 place-items-stretch">
         <div className="bg-white rounded-md shadow-md p-4 w-full text mb-4 flex justify-between flex-col h-full">
           <div>
@@ -64,7 +92,12 @@ function Isi() {
               Masjid Al-Markaz Al-Islami Makassar
             </h1>
           </div>
-          <button className="p-2 bg-green-200">Buka Maps</button>
+          <a
+            href="https://goo.gl/maps/iZWv9x3EPsU3DjzC7"
+            className="p-2 bg-green-200"
+          >
+            Buka Maps
+          </a>
         </div>
         <div className="bg-white rounded-md shadow-md p-4 w-full text mb-4 flex justify-between flex-col h-full">
           <div>
@@ -82,7 +115,12 @@ function Isi() {
               (Penjahit Kiran)
             </h1>
           </div>
-          <button className="p-2 bg-green-200 ">Buka Maps</button>
+          <a
+            href="https://goo.gl/maps/nyH5WmNVkbCErb6r9"
+            className="p-2 bg-green-200 "
+          >
+            Buka Maps
+          </a>
         </div>
       </div>
       <p className="text mt-3 font-semibold ">
