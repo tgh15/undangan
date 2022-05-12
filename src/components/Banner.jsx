9b93861df91f9a3174bd04b035b599a0f7af5467
tgh15 <muhteguhadhiputra15@gmail.com>
2022-05-12 19:19:14 +0800

    fix pretama

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Banner(props) {
   return (
     <section
-      className={` transition-all duration-500 ease-in h-screen w-full  banner text-black flex items-center justify-center flex-col`}
+      className={`${props.className} transition-all duration-500 ease-in h-screen w-full  banner text-black flex items-center justify-center flex-col`}
     >
       <h1 className="nama drop-shadow-xl text-8xl text-white">Erwin & Raisa</h1>
       <h1 className="text drop-shadow-xl text-white text-2xl">
@@ -12,7 +12,7 @@ function Banner(props) {
       <a
         onClick={() => props.btnClick()}
         href="#isi"
-        className="p-3 mt-5 bg-green-400 rounded-md text-white font-bold"
+        className="p-3 mt-5 bg-orange-400 rounded-md text-white font-bold"
       >
         Buka Undangan
       </a>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,18 @@ import "animate.css";
 import music from "../assets/music.mp3";
 import Banner from "../components/Banner";
 import Isi from "../components/Isi";
+import { useState } from "react";
 
 function Home() {
+  const [pop, setPop] = useState("right");
   const playSound = () => {
     let audio = new Audio(music);
+    setPop("left");
     audio.play();
   };
   return (
     <>
-      <Banner btnClick={playSound} /> <Isi />
+      <Banner className={pop} btnClick={playSound} /> <Isi />
     </>
   );
 }
